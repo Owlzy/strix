6cc3c15abe8a1dc359fb1a76fbd1fe7516e7062f
Owlzy <9079651+Owlzy@users.noreply.github.com>
2026-09-09 16:01:19 +0100

    feat: added deltaTime calculation and public getter

--- src/app/Application.ts
+++ src/app/Application.ts
@@ -1,16 +1,29 @@
 import {Renderer} from '../rendering';
 
 export class Application {
+    // getters / setters
+    public get deltaTime() {
+        return this._deltaTime;
+    }
+
     // public fields
     public readonly renderer: Renderer;
 
+    // private fields
+    private lastTime: number;
+    private _deltaTime: number = 0;
+
     constructor(canvas?: HTMLCanvasElement) {
         this.renderer = new Renderer(canvas);
+        this.lastTime = performance.now();
 
         requestAnimationFrame(this.update);
     }
 
     private update = (time: number) => {
+        this._deltaTime = (time - this.lastTime) / 1000;
+        this.lastTime = time;
+
         this.renderer.render();
         requestAnimationFrame(this.update);
     }
