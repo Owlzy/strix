2f34468ae65fca1868305d5c3dbac8d0e9ffeb6a
Owlzy <9079651+Owlzy@users.noreply.github.com>
2026-09-09 18:19:22 +0100

    feat: adding mat3 and refactored Vector2 to not be in a folder as there is unlikely to be a Vector3

--- src/graph/Node.ts
+++ src/graph/Node.ts
@@ -3,7 +3,11 @@ import {Vector2} from "../math";
 export class Node {
     // public fields
     public position: Vector2 = new Vector2();
+    public children: Node[] = [];
 
-    constructor() {
+    public updateTransforms(){
+        for(const c of this.children){
+            c.updateTransforms();
+        }
     }
 }
