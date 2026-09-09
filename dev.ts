f5f5f67b0a8b05f3c21531141101d90f5f23055b
Owlzy <9079651+Owlzy@users.noreply.github.com>
2026-09-09 20:07:00 +0100

    refactor: removed BaseTexture in favour of just a single texture class

--- dev.ts
+++ dev.ts
@@ -1,5 +1,7 @@
-import { Application } from "./src";
-import { Color } from "./src/color";
+import {Application} from "./src";
+import {Color} from "./src/color";
+import {Sprite} from "./src/graph/Sprite";
+import {Vector2} from "./src/math";
 
 const app = new Application();
 document.body.appendChild(app.renderer.canvas);
@@ -8,3 +10,7 @@ app.renderer.canvas.width = screen.width;
 app.renderer.canvas.height = screen.height;
 
 app.renderer.clearColor = Color.BLACK;
+
+const sprite = new Sprite();
+sprite.position = new Vector2(100, 100);
+app.root.add(sprite);
