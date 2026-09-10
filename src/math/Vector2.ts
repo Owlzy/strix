d038a8749e24cab03647deb6502d24b993b90040
Owlzy <9079651+Owlzy@users.noreply.github.com>
2026-09-10 16:13:50 +0100

    fix: x, y now correctly defaults to 0,0, not 0,1

--- src/math/Vector2.ts
+++ src/math/Vector2.ts
@@ -2,7 +2,7 @@ export class Vector2 {
     public x: number = 0;
     public y: number = 0;
 
-    constructor(x: number = 0, y: number = 1) {
+    constructor(x: number = 0, y: number = 0) {
         this.x = x;
         this.y = y;
     }
